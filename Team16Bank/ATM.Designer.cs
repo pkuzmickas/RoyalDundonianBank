f857a8a2cb137bea8c701370c30194442a5fa39c
RonyPortelli <RonyPortelli@computing.dundee.ac.uk>
2017-03-11 15:26:20 +0000

    ITS FINE

--- Team16Bank/ATM.Designer.cs
+++ Team16Bank/ATM.Designer.cs
@@ -28,19 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(4, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(427, 236);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(8, 8);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(427, 236);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(212, 255);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(218, 28);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Team16Bank.Properties.Resources.ATMkeyPad;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 255);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 144);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // ATM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "ATM";
             this.Text = "ATM";
+            this.Load += new System.EventHandler(this.ATM_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Team16Bank/ATM.cs
+++ Team16Bank/ATM.cs
@@ -12,9 +12,77 @@ namespace Team16Bank
 {
     public partial class ATM : Form
     {
+        private PictureBox[,] screenButtons = new PictureBox[4, 2];
+        private PictureBox[,] keyPadButtons = new PictureBox[4, 4];
+        private Label account = new Label();
         public ATM()
         {
             InitializeComponent();
         }
+
+        private void ATM_Load(object sender, EventArgs e)
+        {
+            int horizotal = 28;
+            int vertical = 271;
+            int fileNumber = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                     Image image = Image.FromFile(@"keyPadButtons\key"+ fileNumber +".png");
+                    //if(fileNumber==12 || fileNumber==4 || fileNumber ==8 || fileNumber==16) image.
+                    keyPadButtons[i, j] = new PictureBox();
+                    keyPadButtons[i, j].Size = new Size(32, 25);
+                    keyPadButtons[i, j].Location = new Point(horizotal, vertical);
+                    keyPadButtons[i, j].Tag = new TagInfo { name = "screenButton", value = 0 };
+                   keyPadButtons[i, j].Image = image;
+                    //keyPadButtons[i, j].BackColor = Color.Transparent;
+
+                    /*screenButtons[i,j].BackColor = System.Drawing.Color.Transparent;
+                    screenButtons[i, j].BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+                    screenButtons[i, j].Image = image;
+                    screenButtons[i, j].Location = new System.Drawing.Point(horizotal, vertical);
+                    screenButtons[i, j].Name = "pictureBox1";
+                    screenButtons[i, j].Size = new System.Drawing.Size(52, 40);
+                    screenButtons[i, j].TabIndex = 0;
+                    screenButtons[i, j].TabStop = false;
+                    screenButtons[i, j].Click += new System.EventHandler(screenButtonsClick);*/
+                    if ((j + 1) % 4 == 0)
+                    {
+                        vertical += 30;
+                        horizotal = 28;
+                    }
+                    else
+                    {
+                        if (j == 2) horizotal += 44;
+                        else horizotal += 37;
+                    }
+                    keyPadButtons[i, j].Show();
+                    this.Controls.Add(keyPadButtons[i, j]);
+                    this.Controls.SetChildIndex(keyPadButtons[i, j], 0);
+                    keyPadButtons[i, j].Click += new EventHandler(screenButtonsClick);
+                    fileNumber++;
+                }
+            }
+            keyPadButtons[0, 3].SizeMode = PictureBoxSizeMode.StretchImage;
+            keyPadButtons[0, 3].Size = new Size(37, 25);
+            keyPadButtons[1, 3].SizeMode = PictureBoxSizeMode.StretchImage;
+            keyPadButtons[1, 3].Size = new Size(37, 25);
+            keyPadButtons[2, 3].SizeMode = PictureBoxSizeMode.StretchImage;
+            keyPadButtons[2, 3].Size = new Size(37, 25);
+            keyPadButtons[3, 3].SizeMode = PictureBoxSizeMode.StretchImage;
+            keyPadButtons[3, 3].Size = new Size(37, 25);
+        }
+        private void screenButtonsClick(object sender, EventArgs e)
+        {
+        
+        }
+    }
+
+    public class TagInfo
+    {
+        public string name;
+        public int value;      
     }
 }
